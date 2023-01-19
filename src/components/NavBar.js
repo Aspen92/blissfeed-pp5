e49d8d94664f757ba2c8fc25ee35a070ee6139d7
Aspen92 <asplundchristofer@gmail.com>
2023-01-19 13:57:52 +0000

    Added routing for home/signin/signup/pagenotfound. Added some temporary css to links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,43 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand className={styles.NavBarBrand}>BlissFeed</Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand className={styles.NavBarBrand}>BlissFeed</Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
               <i class="fa-solid fa-house"></i>
               Home
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
               <i class="fa-solid fa-right-to-bracket"></i>
               Sign in
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signup"
+            >
               <i class="fa-solid fa-user"></i>
               Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
