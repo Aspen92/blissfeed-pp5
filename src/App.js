da589840439b7c4df996bb7197b441d15d0e851d
Aspen92 <asplundchristofer@gmail.com>
2023-02-17 07:37:42 +0000

    Add profile page header

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./context/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
+import ProfilePage from "./pages/profiles/ProfilePage";
 
 function App() {
   const currentUser = useCurrentUser();
