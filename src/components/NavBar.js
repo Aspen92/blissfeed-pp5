47dc18505be038198081b3cee17e1f16773c16dd
Aspen92 <asplundchristofer@gmail.com>
2023-02-19 17:57:49 +0000

    Optimize bootstrap imports, comment out console.logs, refetch posts on user login/logout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,7 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import {
