a9d3782c61774c15d1557c41f36166c86acae50b
Aspen92 <asplundchristofer@gmail.com>
2023-02-27 11:06:16 +0000

    Add comments to Comment.js

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -9,6 +9,7 @@ import styles from "../../styles/Comment.module.css";
 import { useCurrentUser } from "../../context/CurrentUserContext";
 import { axiosRes } from "../../api/axiosDefaults";
 
+// Functional component Comment with props
 const Comment = (props) => {
   const {
     profile_id,
@@ -25,9 +26,12 @@ const Comment = (props) => {
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
 
+  // Function to handle delete comment
   const handleDelete = async () => {
     try {
+      // make a request to delete the comment
       await axiosRes.delete(`/comments/${id}/`);
+      // Update post's comments count
       setPost((prevPost) => ({
         results: [
           {
@@ -36,7 +40,7 @@ const Comment = (props) => {
           },
         ],
       }));
-
+      // Update comments list by removing the deleted comment
       setComments((prevComments) => ({
         ...prevComments,
         results: prevComments.results.filter((comment) => comment.id !== id),
@@ -48,10 +52,12 @@ const Comment = (props) => {
     <>
       <hr />
       <Media>
+        {/* Link to the user profile */}
         <Link to={`/profiles/${profile_id}`}>
           <Avatar src={profile_image} />
         </Link>
         <Media.Body className="align-self-center ml-2">
+          {/* Spans for displaying username and the date of the comment */}
           <span className={styles.Owner}>{owner}</span>
           <span className={styles.Date}>{updated_at}</span>
           {showEditForm ? (
@@ -67,7 +73,9 @@ const Comment = (props) => {
             <p>{content}</p>
           )}
         </Media.Body>
+        {/* Check to see if the current user is the owner of the comment */}
         {is_owner && !showEditForm && (
+          // If true and the edit form is not displayed, MoreDropdown component is rendered
           <MoreDropdown
             handleEdit={() => setShowEditForm(true)}
             handleDelete={handleDelete}
