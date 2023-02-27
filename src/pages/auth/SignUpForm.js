f240fd98dff7c43581aebdfa70bd6905bae3bc9e
Aspen92 <asplundchristofer@gmail.com>
2023-02-27 10:50:50 +0000

    Add comments for SignUpForm.js

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -5,16 +5,11 @@ import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-import {
-  Form,
-  Button,
-  Col,
-  Container,
-  Alert,
-} from "react-bootstrap";
+import { Form, Button, Col, Container, Alert } from "react-bootstrap";
 import axios from "axios";
 import { useRedirect } from "../../hooks/useRedirect";
 
+// Define SignUpForm functional component
 const SignUpForm = () => {
   useRedirect("loggedIn");
   const [signUpData, setSignUpData] = useState({
@@ -35,12 +30,14 @@ const SignUpForm = () => {
     });
   };
 
+  // Handle form submission using the handleSubmit function
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
       await axios.post("/dj-rest-auth/registration/", signUpData);
       history.push("/signin");
     } catch (err) {
+      // Set the errors state if the registration was unsuccessful
       setErrors(err.response?.data);
     }
   };
@@ -48,10 +45,12 @@ const SignUpForm = () => {
   return (
     <Container fluid className={styles.SignInUpContainer}>
       <Col className="my-auto py-2 p-md-2" md={6}>
+        {/* Container for the sign up form */}
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>Sign up</h1>
-
+          {/* Sign up form */}
           <Form onSubmit={handleSubmit}>
+            {/* Username input */}
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
@@ -63,12 +62,13 @@ const SignUpForm = () => {
                 onChange={handleChange}
               />
             </Form.Group>
+            {/* Username input error messages */}
             {errors.username?.map((message, idx) => (
               <Alert variant="warning" key={idx}>
                 {message}
               </Alert>
             ))}
-
+            {/* Password input */}
             <Form.Group controlId="password1">
               <Form.Label className="d-none">Password</Form.Label>
               <Form.Control
@@ -80,12 +80,13 @@ const SignUpForm = () => {
                 onChange={handleChange}
               />
             </Form.Group>
+            {/* Password input error messages */}
             {errors.password1?.map((message, idx) => (
               <Alert key={idx} variant="warning">
                 {message}
               </Alert>
             ))}
-
+            {/* Confirm password input */}
             <Form.Group controlId="password2">
               <Form.Label className="d-none">Confirm password</Form.Label>
               <Form.Control
@@ -97,12 +98,13 @@ const SignUpForm = () => {
                 onChange={handleChange}
               />
             </Form.Group>
+            {/* Confirm password input error messages */}
             {errors.password2?.map((message, idx) => (
               <Alert key={idx} variant="warning">
                 {message}
               </Alert>
             ))}
-
+            {/* Sign up button */}
             <Button
               className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
               onMouseDown={(event) => event.preventDefault()}
@@ -117,7 +119,7 @@ const SignUpForm = () => {
             ))}
           </Form>
         </Container>
-
+        {/* Container for the "Already have an account?" link */}
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signin">
             Already have an account? <span>Sign in</span>
