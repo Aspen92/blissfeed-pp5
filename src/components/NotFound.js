1f13d1101cdca904bc355fd57c250cd22407bba7
Aspen92 <asplundchristofer@gmail.com>
2023-02-27 10:01:35 +0000

    Add comment to NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,6 +3,7 @@ import NoResults from "../assets/no-results.png";
 import styles from "../styles/NotFound.module.css";
 import Asset from "./Asset";
 
+// This component renders a "not found" error message with an no-results image and a custom message
 const NotFound = () => {
   return (
     <div className={styles.NotFound}>
